25492f8d4b1710a0fbee2e9f42689c8eaa53305a
mfernandez1047 <mfernandez21135@columbushs.com>
2019-10-29 22:41:58 -0400

    no jquery

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 $( document ).ready( function() {
-    $( 'button' ).click( function() {
-        $( 'body' ).addClass( 'black' );
-    );
+    $( 'button' ).click( function(){
+      if($('body').hasClass('black')) {
+        $('body').removeClass('black').addClass('white');
+      }
+      else{
+          $('body').removeClass('white').addClass('black');
+      }
+      $('#orb').click( function() {        
+        if ($(this).hasClass('sun')) {
+            $(this).removeClass('sun').addClass('moon');
+        }
+        else {
+            $(this).removeClass('moon').addClass('sun');
+        }
+
+    });if ($('#sky').hasClass('day')) {
+    $('#sky').removeClass('day').addClass('night');
+}
+else {
+    $('#sky').removeClass('night').addClass('day');
+}
+if ($('#moonspot1').hasClass('visible')) {
+    $('#moonspot1').removeClass('visible');
+} else {
+    $('#moonspot1').addClass('visible');
+}
+    });
 });
+
